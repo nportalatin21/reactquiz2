7afe2f3ff688bfb3f209d2c8277a5c990b05e8d4
Nicole Portalatin <nportalatin@xpanxion.com>
2023-05-30 10:33:27 -0400

    Update Quiz Question 12

--- src/AddCalculator.jsx
+++ src/AddCalculator.jsx
@@ -1,14 +1,33 @@
 import React from "react";
+import { useState } from "react";
 
 export const AddCalculator = () => {
+  let [add1, setAdd1] = useState(0);
+  let [add2, setAdd2] = useState(0);
+  let [add3, setAdd3] = useState(0);
+  //let [total, setTotal] = useState('');
 
-    return (
-        <>
-            <button>Add 1</button>
-            <button>Add 2</button>
-            <button>Add 3</button>
-            <br></br>
-            Total: 
-        </>
-    )
-}
+  const onHandleClick = () => {
+    setAdd1(++add1);
+  };
+
+  const onHandlePush = () => {
+    setAdd2((add2 = +add2 + 2));
+  };
+
+  const onHandleChevy = () => {
+    setAdd3((add3 = +add3 + 3));
+  };
+  //   const onHandleReset = () => {
+  //     setAdd('');
+  //     setCounter(0)
+  //   }
+  return (
+    <div className="Main">
+      <button onClick={() => onHandleClick(add1)}>Add 1</button>
+      <button onClick={() => onHandlePush(add2)}>Add 2</button>
+      <button onClick={() => onHandleChevy(add3)}>Add 3</button>
+      Total: {add1 + add2 + add3}
+    </div>
+  );
+};
--- src/NumberBuilder.jsx
+++ src/NumberBuilder.jsx
@@ -1,32 +1,28 @@
 import React from "react";
 import { useState } from "react";
 
-
 export const NumberBuilder = () => {
- let [counter, setCounter] = useState(0);
- let [add, setAdd] = useState('');
+  let [counter, setCounter] = useState(0);
+  let [add, setAdd] = useState("");
 
- const onHandleClick = () => {
+  const onHandleClick = () => {
+    setCounter(++counter);
+    setAdd((add += counter + ""));
+  };
 
- setCounter(++counter)
- setAdd(add +=(counter + ''))
-};
+  const onHandleReset = () => {
+    setAdd("");
+    setCounter(0);
+  };
 
- const onHandleReset = () => {
-setAdd('');
- setCounter(0)
- }
-
- return (
-
- <div className="Main">
-
- <button onClick={() => onHandleClick()}>Add Number</button>
-<span>
- <button onClick={() => onHandleReset()}>Reset</button>
-</span>
-
- <br/>
-  Numbers: {add} 
-</div> );
+  return (
+    <div className="Main">
+      <button onClick={() => onHandleClick()}>Add Number</button>
+      <span>
+        <button onClick={() => onHandleReset()}>Reset</button>
+      </span>
+      <br />
+      Numbers: {add}
+    </div>
+  );
 };
